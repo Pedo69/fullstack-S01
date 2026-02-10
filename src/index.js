24ce5d54c7ee3b7960eb5c1f5171e8ba3378a68f
Pedo69 <apicha.pm2549@gmail.com>
2026-02-10 15:23:07 +0700

    day11

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World Apicha!");
+  res.send("Hello World Apicha! Peechai");
 });
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
